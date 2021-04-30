1b03e401db7dbfd1ebcd1c44e51e57be3e3b1cb8
Abdullah0505 <>
2021-04-30 23:37:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('change-font').addEventListener('click', changeFont)
+
+document.getElementById('change-size').addEventListener('click', changeSize)
+
+document.getElementById('change-color').addEventListener('click', changeColor)
+
+function changeFont (){
+  document.getElementById('paragraph').style.fontFamily = 'Arial'
+}
+
+function changeSize (){
+  document.getElementById('paragraph').style.fontSize = 'X-large'
+}
+
+function changeColor (){
+  document.getElementById('paragraph').style.color = 'red'
+}
